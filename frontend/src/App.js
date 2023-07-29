c5ad15583c27200c4a27555c808e28443c2e6f23
Nikolai <borodinov.nikolai@gmail.com>
2023-07-29 11:44:01 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,6 @@ function App() {
     <div className="App">
       <Router>
       <Header/>
-      <div>Тестовая запись</div>
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/catalog/*' element={<DeviceRouter/>} />
